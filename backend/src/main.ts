912dd7f4c6379250532a65c074a12ec4e63bd0d3
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2024-12-20 19:49:52 -0300

    backend FINALIZADO AEWWW

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Para evitar o conflito de endpoints do front com back
+  app.enableCors()
+
+  // Antes de executar a rota no controller, passará por essa função
+  // para validar os dados seguindo o formato dto
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
