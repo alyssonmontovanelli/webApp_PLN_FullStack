6269446afa2afe1b6bd7cd73843c43eb1faf04ed
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2025-01-03 16:16:21 -0300

    fix: install axios and its types

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,35 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Para evitar o conflito de endpoints do front com back
-  app.enableCors()
+  // Habilitar CORS
+  app.enableCors();
 
-  // Antes de executar a rota no controller, passará por essa função
-  // para validar os dados seguindo o formato dto
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT ?? 3000);
+  // Configurar a validação global de dados
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Configurar a porta com fallback para 3000
+  const port = process.env.PORT || 3000;
+  console.log(`App listening on port: ${port}`);  // Adicionando log para debug
+  await app.listen(port);
 }
-bootstrap()
+bootstrap();
+
+
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   // Para evitar o conflito de endpoints do front com back
+//   app.enableCors()
+
+//   // Antes de executar a rota no controller, passará por essa função
+//   // para validar os dados seguindo o formato dto
+//   app.useGlobalPipes(new ValidationPipe())
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap()
